3e7a86edae988dc49da2ef8f901f2f01a009af7c
Roadom <mrolarte@student.apc.edu.ph>
2025-08-26 01:41:04 +0800

    added dynamic sign up/sign in text

--- lib/pages/auth/auth_page.dart
+++ lib/pages/auth/auth_page.dart
@@ -2,27 +2,42 @@ import 'package:flutter/material.dart';
 import 'package:supabase_auth_ui/supabase_auth_ui.dart';
 import 'package:flutter/foundation.dart';
 
-class AuthPage extends StatelessWidget {
+class AuthPage extends StatefulWidget {
   const AuthPage({super.key});
 
   @override
+  State<AuthPage> createState() => _AuthPageState();
+}
+
+class _AuthPageState extends State<AuthPage> {
+  bool _isSigningIn = true; 
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Login')),
+    return Scaffold(  
       body: Padding(
         padding: const EdgeInsets.all(20.0),
-        child: SupaEmailAuth(
-          redirectTo: kIsWeb ? null : 'io.mydomain.myapp://callback',
-          onSignInComplete: (response) {
-          },
-          onSignUpComplete: (response) {
-          },
-          metadataFields: [],
-        )
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              _isSigningIn ? 'Sign in' : 'Sign up',
+              style: Theme.of(context).textTheme.headlineSmall,
+            ),
+            const SizedBox(height: 12),
+            SupaEmailAuth(
+              isInitiallySigningIn: _isSigningIn,
+              onToggleSignIn: (bool isSigningIn) {
+                setState(() => _isSigningIn = isSigningIn);
+              },
+              redirectTo: kIsWeb ? null : 'io.mydomain.myapp://callback',
+              onSignInComplete: (response) {},
+              onSignUpComplete: (response) {},
+              metadataFields: const [],
+            ),
+          ],
+        ),
       ),
     );
   }
 }
-
-
-
